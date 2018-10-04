a6e91fe1a888c5dff38da0c27be850953325362c
dhruvark <43543905+dhruvark@users.noreply.github.com>
2018-10-04 01:02:58 -0400

    update JWT expiry to 5

--- iot/api-client/end_to_end_example/cloudiot_pubsub_example_mqtt_device.py
+++ iot/api-client/end_to_end_example/cloudiot_pubsub_example_mqtt_device.py
@@ -235,7 +235,7 @@ def parse_command_line_args():
               'telemetry event or a device state message.'))
     parser.add_argument(
         '--jwt_expires_minutes',
-        default=1,
+        default=3,
         type=int,
         help=('Expiration time, in minutes, for JWT tokens.'))
 
